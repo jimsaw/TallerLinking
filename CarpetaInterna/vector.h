ae37347d3e4a719c927a3139985c2c8377b8d927
jimsaw <lfmonten@espol.edu.ec>
2017-11-15 09:47:24 -0500

    terminada la segunda parte del taller#4

--- CarpetaInterna/vector.h
+++ CarpetaInterna/vector.h
@@ -1,3 +1,8 @@
+typedef struct Vector{
+	int a[2];
+}vector;
+
+
 void addvec(int *x, int *y,
 	    int *z, int n);
 void multvec(int *x, int *y, 
--- main2.c
+++ main2.c
@@ -10,10 +10,40 @@ int w[2];
 
 int main() 
 {
+    vector v1,v2,vfinalsuma,vfinalmult;
+
+printf("===============Taller #4===============\n\n");
+
+    printf("******************\n"); 
+    printf("* Primera Parte: *\n");
+    printf("******************\n"); 
     addvec(x, y, z, 2);
     printf("z = [%d %d]\n", z[0], z[1]);
     multvec(x, y, w, 2);
     printf("w = [%d %d]\n", w[0], w[1]);
+
+    printf("\n\n");
+
+    printf("******************\n");
+    printf("* Segunda Parte: *\n");
+    printf("******************\n");
+    printf("Ingrese los valores x, y para el vector1 y para el vector2: \n");
+    printf("Para el vector1:\n");
+    scanf("%d",&v1.a[0]);
+    scanf("%d",&v1.a[1]);
+    printf("Para el vector2:\n");
+    scanf("%d",&v2.a[0]); 
+    scanf("%d",&v2.a[1]); 
+    addvec(v1.a,v2.a,vfinalsuma.a,2);
+    printf("\nVector resultado de sumar el vector1 y el vector2 es igual a [%d %d]\n", vfinalsuma.a[0], vfinalsuma.a[1]);
+    multvec(v1.a,v2.a,vfinalmult.a,2);
+    printf("Vector resultado de multiplicar el vector1 y el vector2 es igual a [%d %d]\n\n", vfinalmult.a[0], vfinalmult.a[1]);    
+
+
+
+
+
+
     return 0;
 }
 /* $end main2 */
